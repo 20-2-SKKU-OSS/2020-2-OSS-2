9d5e58be7ed213db5785ea03259ad12c847f35ac
Yanghyeondong <hdyang0686@naver.com>
2020-11-24 18:36:42 +0900

    [add] 국가이름 한국 일부추가 및 미국, 중국, 일본 추가

--- utils/transformName.js
+++ utils/transformName.js
@@ -1,10 +1,33 @@
 
 module.exports = async (countryName) => {
 
-    if(countryName == "korea" || countryName == "Korea" || countryName == "S.Korea" || countryName == "ROK" || 
+    //S. korea
+    if(countryName == "korea" || countryName == "Korea" || countryName == "S.Korea" || countryName == "Korea, Republic of" || countryName == "ROK" || 
        countryName == "southkorea" || countryName == "SouthKorea" || countryName == "Hanguk" || countryName == "hanguk"){
+
         return "S. korea";
     }
+
+    //USA
+    else if(countryName == "UnitedStates" || countryName == "U.S." || countryName == "UnitedStatesofAmerica"){
+
+        return "usa";
+    }
+
+    //China
+    //PRC = People's Republic of China
+    else if(countryName == "PRC" || countryName == "People's Republic of China" || countryName == "People'sRepublicofChina"){
+
+        return "China";
+    }
+
+    //Japan
+    else if(countryName == "State of Japan" || countryName == "Nihon"){
+
+        return "Japan";
+    }
+
+
     else {
         return countryName;
     }
