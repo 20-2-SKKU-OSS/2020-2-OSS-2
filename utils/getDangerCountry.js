75cafe29cd9e2b82a749f71ae28abe9dca9b4328
Yanghyeondong <hdyang0686@naver.com>
2020-11-29 16:22:46 +0900

    [add] 위험국가 테이블 출력 함수 완성
    각 국가별로 최근 히스토리를 모두 검색한 결과 시간이 너무 오래걸려서 현재 확진자 permillion 으로 지표 결정.

--- utils/getDangerCountry.js
+++ utils/getDangerCountry.js
@@ -12,7 +12,67 @@ module.exports = async (
 	output,
 	states,
 	countryName,
-	{ sortBy, limit, reverse, bar, json, continent }
+	{ sortBy, limit, reverse, bar, json, continent, danger }
 ) => {
-    
+	if (!countryName && !states && !bar && !continent && danger) {
+		sortValidation(sortBy, spinner);
+		const [err, response] = await to(
+			axios.get(`https://corona.lmao.ninja/v2/countries`)
+		);
+		handleError(`API is down, try again later.`, err, false);
+		let allCountries = response.data;
+
+		// Format.
+		const format = numberFormat(json);
+
+		// Sort & reverse.
+		const direction = reverse ? 'asc' : 'desc';
+		allCountries = orderBy(
+			allCountries,
+			[sortingKeys[sortBy]],
+			[direction]
+		);
+
+        //get worldwide
+        const [err2, response2] = await to(
+            axios.get(`https://corona.lmao.ninja/v2/all`)
+        );
+        handleError(`API is down, try again later.`, err2, false);
+
+        let allData = response2.data;
+        let worldper = allData.casesPerOneMillion;
+        let realCount = 0;
+
+		// Limit.
+		allCountries = allCountries.slice(0, limit);
+
+		// Push selected data.
+		allCountries.map((oneCountry, count) => {
+
+            if (oneCountry.casesPerOneMillion > worldper * 3){
+                realCount++;
+                output.push([
+                    realCount,
+                    oneCountry.country,
+                    format(oneCountry.cases),
+                    format(oneCountry.todayCases),
+                    format(oneCountry.deaths),
+                    format(oneCountry.todayDeaths),
+                    format(oneCountry.recovered),
+                    format(oneCountry.active),
+                    format(oneCountry.critical),
+                    format(oneCountry.casesPerOneMillion)
+                ]);
+            }
+		});
+
+
+		spinner.stopAndPersist();
+		const isRev = reverse ? `${dim(` & `)}${cyan(`Order`)}: reversed` : ``;
+		if (!json) {
+            spinner.info(`${cyan(`About Dangerous Country`)}`);
+		}
+		console.log(output.toString());
+	}
+
 };
