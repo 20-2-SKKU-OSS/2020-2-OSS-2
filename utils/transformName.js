0700b64579fd5ff2148c06bb32d7e86776589b7e
Yanghyeondong <hdyang0686@naver.com>
2020-12-04 14:38:00 +0900

    [add] 국가이름 변경함수 사용 및 주의사항 주석추가

--- utils/transformName.js
+++ utils/transformName.js
@@ -1,8 +1,13 @@
 
 module.exports = async (countryName) => {
 
-    //Sources of all country names are referenced at "en.wikipedia.org".
-
+    /*
+        1. Sources of all country names are referenced at "en.wikipedia.org".
+        2. Return value should always be a standardized (default output to corona-cli) country name.
+        The countries currently supported are:
+            S. korea, USA, China, Japan, Russia, United Kingdom, Germany
+    */
+   
     //S. korea
     if(countryName == "korea" || countryName == "Korea" || countryName == "S.Korea" || countryName == "Korea, Republic of" || countryName == "ROK" || 
        countryName == "southkorea" || countryName == "SouthKorea" || countryName == "Hanguk" || countryName == "hanguk"){
@@ -46,6 +51,7 @@ module.exports = async (countryName) => {
         return "Germany";
     }
 
+    //Not applicable to transform.
     else {
         return countryName;
     }
